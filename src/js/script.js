b17f0ed8b8ecdcd731250fdfe689b2208739bc21
AdiSan103 <arwinsmith01@gmail.com>
2023-06-24 21:16:40 +0800

    pembaharuan

--- src/js/script.js
+++ src/js/script.js
@@ -47,3 +47,21 @@ var swiper = new Swiper(".beranda-mySwiper-news", {
     },
   },
 });
+
+// button to up
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollButton").style.display = "block";
+  } else {
+    document.getElementById("scrollButton").style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // Untuk Safari
+  document.documentElement.scrollTop = 0; // Untuk Chrome, Firefox, IE, dan Opera
+}
